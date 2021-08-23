25d63848ce919f6da14e98155912554ccb131737
viiviii <viiiix.viiviii@gmail.com>
2021-08-23 14:41:45 +0900

    :white_check_mark: test: 테스트 보강

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -7,13 +7,22 @@ public class BoardTest {
     @Test
     public void isFull() throws Exception {
         //given
-        EmptySpace emptySpace = new EmptySpace();
-        Board board = new Board(emptySpace);
-
-        //when
-        boolean isFull = board.isFull();
+        EmptySpace emptySpace1 = new EmptySpace();
+        EmptySpace emptySpace2 = new EmptySpace(new Offset(1, 1));
+        Board board1 = Board.from(emptySpace1);
+        Board board2 = Board.from(emptySpace2);
 
         //then
-        assertTrue(isFull);
+        assertTrue(board1.isFull());
+        assertFalse(board2.isFull());
+    }
+    
+    @Test
+    public void 인자로_null이_올_경우_UNFIT을_리턴한다() throws Exception {
+        //given
+        Board board = Board.from(null);
+        
+        //then
+        assertEquals(board, Board.UNFIT);
     }
 }
