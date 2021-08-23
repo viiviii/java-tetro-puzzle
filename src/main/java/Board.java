bf93f8461a1fc60994e8205d311de73bb0f2d272
viiviii <viiiix.viiviii@gmail.com>
2021-08-23 14:41:18 +0900

    :sparkles: feature: Board 클래스 추가

---
+++ src/main/java/Board.java
@@ -0,0 +1,18 @@
+
+public class Board {
+    public static final Board UNFIT = new Board(new EmptySpace(new Offset(-99999, -99999)));
+    private final EmptySpace emptySpace;
+
+    private Board(EmptySpace emptySpace) {
+        this.emptySpace = emptySpace;
+    }
+
+    public static Board from(EmptySpace emptySpace) {
+        if (emptySpace == null) return UNFIT;
+        return new Board(emptySpace);
+    }
+
+    public boolean isFull() {
+        return emptySpace.size() == 0;
+    }
+}
