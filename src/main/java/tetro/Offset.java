2cf1a876f9d4798a0f70378cb4e36d09583e9e91
viiviii <viiiix.viiviii@gmail.com>
2021-08-23 18:27:46 +0900

    :recycle: refactor: Offset 객체를 cache하도록 변경

--- src/main/java/tetro/Offset.java
+++ src/main/java/tetro/Offset.java
@@ -4,13 +4,35 @@ package tetro;
  * 좌표의 x, y 위치를 나타낸다
  */
 // TODO: 이펙티브 자바 equals를 재정의하려거든 hashCode도 재정의하라
+// TODO: cache 배열에서 가져오면 equals, compareTo, hashCode, ...도 변경하는게 맞는건가?
 public final class Offset implements Comparable<Offset> {
-    final int x;
-    final int y;
+    public final int x;
+    public final int y;
 
-    public Offset(int x, int y) {
+    public static final Offset INVALID = new Offset(-99999, -99999);
+    private static final int LENGTH = Board.LENGTH;
+
+    private static final Offset[] cache = new Offset[LENGTH * LENGTH];
+
+    static {
+        for (int x = 0; x < LENGTH; x++) {
+            for (int y = 0; y < LENGTH; y++) {
+                cache[index(x, y)] = new Offset(x, y);
+            }
+        }
+    }
+
+    private Offset(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public static Offset of(int x, int y) {
+        return cache[index(x, y)];
+    }
+
+    private static Integer index(int x, int y) {
+        return (x * LENGTH) + y;
     }
 
     @Override
