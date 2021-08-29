ceec61eab012872c46b679f7e6cbb9a4324ff851
viiviii <viiiix.viiviii@gmail.com>
2021-08-29 20:26:07 +0900

    :recycle: refactor: Offset hashCode overrid
    - HashSet을 사용하지 않을거라 override 하지않고 임시로 뒀었지만
    - ImmutableSet에서도 올바른 비교가 되지 않아 override 함
    - ImmutableSet contains의 probe()에서 hashCode를 사용하여 index를 구하는데
      - 이때 hashCode를 override 하지 않았을 경우 올바른 index를 구하지 못해 값 비교 실패함

--- src/main/java/tetro/Offset.java
+++ src/main/java/tetro/Offset.java
@@ -1,9 +1,10 @@
 package tetro;
 
+import java.util.Objects;
+
 /**
  * 좌표의 x, y 위치를 나타낸다
  */
-// TODO: 이펙티브 자바 equals를 재정의하려거든 hashCode도 재정의하라
 public final class Offset implements Comparable<Offset> {
     public final int x;
     public final int y;
@@ -16,6 +17,11 @@ public final class Offset implements Comparable<Offset> {
     // TODO: 기존 코드와 호환을 위해 임시로 둠
     public static Offset of(int x, int y) {
         return new Offset(x, y);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 
     @Override
