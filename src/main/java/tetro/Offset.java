fc5c807873b395a05eaab15bda1b3f489506cc5a
viiviii <viiiix.viiviii@gmail.com>
2021-08-29 15:18:56 +0900

    :fire: remove: Offset cache 기능 제거
    - x, y 값만 나타내야 되는데 보드 크기로 제한 등 많은 걸 하는것 같아서 제거함

--- src/main/java/tetro/Offset.java
+++ src/main/java/tetro/Offset.java
@@ -1,48 +1,21 @@
 package tetro;
 
-import tetro.block.BlockShape;
-import tetro.exception.OffsetOutOfBoundsException;
-
 /**
  * 좌표의 x, y 위치를 나타낸다
  */
 // TODO: 이펙티브 자바 equals를 재정의하려거든 hashCode도 재정의하라
-// TODO: cache 배열에서 가져오면 equals, compareTo, hashCode, ...도 변경하는게 맞는건가?
 public final class Offset implements Comparable<Offset> {
     public final int x;
     public final int y;
-
-    public static final Offset INVALID = new Offset(-99999, -99999);
-    public static final int LENGTH = Board.LENGTH;
-
-    private static final Offset[] cache = new Offset[LENGTH * LENGTH];
-
-    static {
-        for (int x = 0; x < LENGTH; x++) {
-            for (int y = 0; y < LENGTH; y++) {
-                cache[index(x, y)] = new Offset(x, y);
-            }
-        }
-    }
 
     private Offset(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    // TODO: 기존 코드와 호환을 위해 임시로 둠
     public static Offset of(int x, int y) {
-        return cache[index(x, y)];
-    }
-
-    private static Integer index(int x, int y) {
-        if (invalidBounds(x, y)) {
-            throw new OffsetOutOfBoundsException(x, y);
-        }
-        return x + (y * LENGTH);
-    }
-
-    private static boolean invalidBounds(int x, int y) {
-        return x < 0 || y < 0 || x >= LENGTH || y >= LENGTH;
+        return new Offset(x, y);
     }
 
     @Override
