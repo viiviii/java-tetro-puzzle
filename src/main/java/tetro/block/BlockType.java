fec0f419b0b769c92dd7d709938777423c98b5f3
viiviii <viiiix.viiviii@gmail.com>
2021-08-29 20:50:51 +0900

    :sparkles: feature: BlockType 별 회전한 도형 갯수 추가

--- src/main/java/tetro/block/BlockType.java
+++ src/main/java/tetro/block/BlockType.java
@@ -3,18 +3,22 @@ package tetro.block;
 import tetro.Offset;
 
 public enum BlockType {
-    I(0, 0, 0, 1, 0, 2, 0, 3),
-    O(0, 0, 1, 0, 0, 1, 1, 1),
-    T(0, 0, 1, 0, 2, 0, 1, 1),
-    S(1, 0, 2, 0, 0, 1, 1, 1),
-    Z(0, 0, 1, 0, 1, 1, 2, 1),
-    J(1, 0, 1, 1, 0, 2, 1, 2),
-    L(0, 0, 0, 1, 0, 2, 1, 2);
+    O(1,0, 0, 1, 0, 0, 1, 1, 1),
+    I(2,0, 0, 0, 1, 0, 2, 0, 3),
+    S(2,1, 0, 2, 0, 0, 1, 1, 1),
+    Z(2,0, 0, 1, 0, 1, 1, 2, 1),
+    T(4,0, 0, 1, 0, 2, 0, 1, 1),
+    J(4,1, 0, 1, 1, 0, 2, 1, 2),
+    L(4,0, 0, 0, 1, 0, 2, 1, 2);
 
+    public final int numberOfRotatedShapes;
     public final BlockShape shape;
 
+
     // TODO: o1x, o1y, ... 이 부분
-    BlockType(int o1x, int o1y, int o2x, int o2y, int o3x, int o3y, int o4x, int o4y) {
+    // TODO: numberOfRotatedShapes 변수명
+    BlockType(int numberOfRotatedShapes, int o1x, int o1y, int o2x, int o2y, int o3x, int o3y, int o4x, int o4y) {
+        this.numberOfRotatedShapes = numberOfRotatedShapes;
         final Offset o1 = Offset.of(o1x, o1y);
         final Offset o2 = Offset.of(o2x, o2y);
         final Offset o3 = Offset.of(o3x, o3y);
