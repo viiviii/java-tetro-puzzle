97b29180c7d17ada8622db5a1d015a4a82e12062
viiviii <viiiix.viiviii@gmail.com>
2021-08-30 01:08:23 +0900

    :sparkles: feature: Offset implements Rotatable

--- src/main/java/tetro/Offset.java
+++ src/main/java/tetro/Offset.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 /**
  * 좌표의 x, y 위치를 나타낸다
  */
-public final class Offset implements Comparable<Offset> {
+public final class Offset implements Comparable<Offset>, Rotatable<Offset> {
     public static final Offset INVALID = Offset.of(Integer.MIN_VALUE, Integer.MAX_VALUE);
     public static final Offset ORIGIN = Offset.of(1, 1);
 
@@ -45,5 +45,24 @@ public final class Offset implements Comparable<Offset> {
     @Override
     public String toString() {
         return "Offset{" + "x=" + x + ", y=" + y + '}';
+    }
+
+    // TODO: 이하 메서드 테스트 코드 작성 및 테스트 필요
+    @Override
+    public Offset rotate() {
+        return quarterTurn();
+    }
+
+    // 0: (x, y) -> 1: (-y, x) -> 2: (-x, -y) -> 3: (y, -x) -> 4 == 0: (x, y)
+    private Offset quarterTurns(int quarterTurns) {
+        final int MAX_ROTATIONS = 360 / 90;
+        final int turns = quarterTurns % MAX_ROTATIONS;
+        if (turns < 1) return this;
+        final Offset rotateOffset = quarterTurn();
+        return rotateOffset.quarterTurns(turns - 1);
+    }
+
+    private Offset quarterTurn() {
+        return Offset.of(-this.y, this.x);
     }
 }
