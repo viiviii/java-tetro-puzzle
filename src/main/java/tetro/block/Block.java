bb44f11b923deb4ae103295fcce58fdd2768cca6
viiviii <viiiix.viiviii@gmail.com>
2021-08-30 16:02:24 +0900

    :sparkles: feature: Block 클래스 추가

---
+++ src/main/java/tetro/block/Block.java
@@ -0,0 +1,26 @@
+package tetro.block;
+
+public abstract class Block {
+    private final BlockType type;
+    private final BlockShape shape;
+
+    protected Block(BlockType type, BlockShape shape) {
+        this.type = type;
+        this.shape = shape;
+        System.out.println("this = " + this); // TODO: 제거
+    }
+
+    public BlockType type() {
+        return type;
+    }
+
+    public BlockShape shape() {
+        return shape;
+    }
+
+    // TODO
+    @Override
+    public String toString() {
+        return "Block{" + "type=" + type + ", shape=" + shape + '}';
+    }
+}
