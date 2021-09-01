c494f6d5ddfaeca21f0a7c46772a16c6d65b0b6f
viiviii <viiiix.viiviii@gmail.com>
2021-09-01 20:59:24 +0900

    :recycle: refactor: Offsets final class로 변경

--- src/main/java/tetro/offset/Offsets.java
+++ src/main/java/tetro/offset/Offsets.java
@@ -2,41 +2,32 @@ package tetro.offset;
 
 import java.util.*;
 
-public class Offsets {
+// TODO: AbstractSet 상속 YN?
+public final class Offsets {
     private final Set<Offset> offsets;
 
     private Offsets(Set<Offset> offsets) {
         this.offsets = offsets;
     }
 
-    // TODO
-    protected Offsets(Offsets offsets) {
-        this(offsets.toImmutableSet());
+    public static final Offsets from(Set<Offset> set) {
+        return new Offsets(Set.copyOf(set));
     }
 
-    public static final Offsets from(Set<Offset> offsets) {
-        return new Offsets(Set.copyOf(offsets));
-    }
-
-    // TODO
-    public static final Offsets of(Offset o1) {
-        final Set set = Set.of(o1);
-        return new Offsets(set);
+    public static final Offsets of(Offset offset) {
+        return new Offsets(Set.of(offset));
     }
 
     public static final Offsets of(Offset o1, Offset o2) {
-        final Set set = Set.of(o1, o2);
-        return new Offsets(set);
+        return new Offsets(Set.of(o1, o2));
     }
 
     public static final Offsets of(Offset o1, Offset o2, Offset o3) {
-        final Set set = Set.of(o1, o2, o3);
-        return new Offsets(set);
+        return new Offsets(Set.of(o1, o2, o3));
     }
 
     public static final Offsets of(Offset o1, Offset o2, Offset o3, Offset o4) {
-        final Set set = Set.of(o1, o2, o3, o4);
-        return new Offsets(set);
+        return new Offsets(Set.of(o1, o2, o3, o4));
     }
 
     public final Set<Offset> toImmutableSet() {
