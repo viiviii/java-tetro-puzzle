fbe1a2103ef3be2379ef1cf9541a7aa195fb6d55
viiviii <viiiix.viiviii@gmail.com>
2021-09-02 12:37:03 +0900

    :recycle: refactor: Offsets SortedSet 사용하도록 변경

--- src/main/java/tetro/offset/Offsets.java
+++ src/main/java/tetro/offset/Offsets.java
@@ -2,36 +2,39 @@ package tetro.offset;
 
 import java.util.*;
 
-// TODO: AbstractSet 상속 YN?
 public final class Offsets {
-    private final Set<Offset> offsets;
+    private final SortedSet<Offset> offsets;
 
-    private Offsets(Set<Offset> offsets) {
-        this.offsets = offsets;
+    private Offsets(List<Offset> offsets) {
+        this.offsets = new TreeSet(offsets);
     }
 
     public static final Offsets from(Set<Offset> set) {
-        return new Offsets(Set.copyOf(set));
+        return new Offsets(List.copyOf(set));
     }
 
     public static final Offsets of(Offset offset) {
-        return new Offsets(Set.of(offset));
+        return new Offsets(List.of(offset));
     }
 
     public static final Offsets of(Offset o1, Offset o2) {
-        return new Offsets(Set.of(o1, o2));
+        return new Offsets(List.of(o1, o2));
     }
 
     public static final Offsets of(Offset o1, Offset o2, Offset o3) {
-        return new Offsets(Set.of(o1, o2, o3));
+        return new Offsets(List.of(o1, o2, o3));
     }
 
     public static final Offsets of(Offset o1, Offset o2, Offset o3, Offset o4) {
-        return new Offsets(Set.of(o1, o2, o3, o4));
+        return new Offsets(List.of(o1, o2, o3, o4));
     }
 
     public int size() {
         return this.offsets.size();
+    }
+
+    public Offset first() {
+        return this.offsets.first();
     }
 
     @Override
