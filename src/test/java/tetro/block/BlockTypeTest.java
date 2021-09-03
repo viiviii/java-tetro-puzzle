284e53a9462d8fb6df6e1f4daa01856cd888a31a
viiviii <viiiix.viiviii@gmail.com>
2021-09-03 17:06:13 +0900

    :recycle: refactor: BlockTypeTest 리팩토링

--- src/test/java/tetro/block/BlockTypeTest.java
+++ src/test/java/tetro/block/BlockTypeTest.java
@@ -3,31 +3,37 @@ package tetro.block;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.function.Predicate;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BlockTypeTest {
-    private final BlockType[] BLOCK_TYPES = BlockType.values();
 
+    /**
+     * 테트리스 회전은 최대 4번까지 가능하므로 블럭 모양은 최소 1개에서 최대 4개이다.
+     */
     @Test
-    public void numberOfRotatedShapes_범위는_1_이상_4_이하이다() throws Exception {
+    public void numberOfShapes_rangeIsBetween1And4_returnsTrue() throws Exception {
         //when
-        boolean allMatched = Arrays.stream(BLOCK_TYPES).allMatch(e -> {
-            int num = e.numberOfRotatedShapes;
-            return num >= 1 && num <= 4;
-        });
+        boolean actual = blockTypesMatchAll(e -> e.numberOfShapes >=1 && e.numberOfShapes <= 4);
 
         //then
-        assertTrue(allMatched);
+        assertTrue(actual);
     }
 
+    /**
+     * 테트리스 블록은 4개의 칸으로 구성되어 있다.
+     */
     @Test
-    public void blockType_offsets의_size는_4여야_한다() throws Exception {
+    public void offsetsSize_equalsNumberFour_returnsTrue() throws Exception {
         //when
-        boolean allMatched = Arrays.stream(BLOCK_TYPES)
-                .allMatch(e -> e.offsets.size() == 4);
+        boolean actual = blockTypesMatchAll(e -> e.offsets.size() == 4);
 
         //then
-        assertTrue(allMatched);
+        assertTrue(actual);
+    }
+
+    private boolean blockTypesMatchAll(Predicate<BlockType> predicate) {
+        return Arrays.stream(BlockType.values()).allMatch(predicate);
     }
 }
