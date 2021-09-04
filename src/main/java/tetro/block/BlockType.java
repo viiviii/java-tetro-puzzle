18cbf66578ef51c61c7de8c40854bbcb429b3ba7
viiviii <viiiix.viiviii@gmail.com>
2021-09-04 13:12:09 +0900

    :recycle: refactor: BlockType이 shapes를 갖고있도록

--- src/main/java/tetro/block/BlockType.java
+++ src/main/java/tetro/block/BlockType.java
@@ -1,27 +1,35 @@
 package tetro.block;
 
-import tetro.offset.Offset;
-import tetro.offset.Offsets;
+import tetro.shape.BlockShape;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public enum BlockType {
-    O(1,0, 0, 1, 0, 0, 1, 1, 1),
-    I(2,0, 0, 0, 1, 0, 2, 0, 3),
-    S(2,1, 0, 2, 0, 0, 1, 1, 1),
-    Z(2,0, 0, 1, 0, 1, 1, 2, 1),
-    T(4,0, 0, 1, 0, 2, 0, 1, 1),
-    J(4,1, 0, 1, 1, 0, 2, 1, 2),
-    L(4,0, 0, 0, 1, 0, 2, 1, 2);
+    O(1, 0, 0, 1, 0, 0, 1, 1, 1),
+    I(2, 0, 0, 0, 1, 0, 2, 0, 3),
+    S(2, 1, 0, 2, 0, 0, 1, 1, 1),
+    Z(2, 0, 0, 1, 0, 1, 1, 2, 1),
+    T(4, 0, 0, 1, 0, 2, 0, 1, 1),
+    J(4, 1, 0, 1, 1, 0, 2, 1, 2),
+    L(4, 0, 0, 0, 1, 0, 2, 1, 2);
 
-    public final int numberOfShapes;
-    public final Offsets offsets;
+    public final BlockShapes shapes;
 
-    // TODO: o1x, o1y, ... 이 부분
-    BlockType(int numberOfShapes, int o1x, int o1y, int o2x, int o2y, int o3x, int o3y, int o4x, int o4y) {
-        this.numberOfShapes = numberOfShapes;
-        final Offset o1 = Offset.of(o1x, o1y);
-        final Offset o2 = Offset.of(o2x, o2y);
-        final Offset o3 = Offset.of(o3x, o3y);
-        final Offset o4 = Offset.of(o4x, o4y);
-        this.offsets = Offsets.of(o1, o2, o3, o4);
+    BlockType(int numberOfShapes, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
+        this.shapes = BlockShapes.of(numberOfShapes, x1, y1, x2, y2, x3, y3, x4, y4);
+    }
+
+    private static class BlockShapes {
+        final List<BlockShape> shapes;
+
+        private BlockShapes(List<BlockShape> shapes) {
+            this.shapes = shapes;
+        }
+
+        // TODO
+        private static BlockShapes of(int numberOfShapes, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
+            return new BlockShapes(new ArrayList<BlockShape>());
+        }
     }
 }
