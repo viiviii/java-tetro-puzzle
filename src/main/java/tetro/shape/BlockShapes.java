7a52d204e2d45c29d1fb76ecef360fa3c4c6d531
viiviii <viiiix.viiviii@gmail.com>
2021-09-05 12:30:01 +0900

    :recycle: refactor: BlockShapes static block 리팩토링

--- src/main/java/tetro/shape/BlockShapes.java
+++ src/main/java/tetro/shape/BlockShapes.java
@@ -3,24 +3,30 @@ package tetro.shape;
 import tetro.block.BlockType;
 import tetro.offset.Offsets;
 
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 // TODO: BlockShape.from(shapeOffsets).rotate(rotation).create();
 public final class BlockShapes {
     private static final Set<BlockShape> shapes;
 
     static {
-        Set<BlockShape> allShapes = new HashSet<>();
-        BlockType[] allBlockTypes = BlockType.values();
-        for (BlockType type : allBlockTypes) {
-            Offsets shapeOffsets = type.offsets;
-            for (int rotation = 0; rotation < type.numberOfShapes; rotation++) {
-                BlockShape shape = BlockShape.from(shapeOffsets).rotate();
-                allShapes.add(shape);
-            }
-        }
-        shapes = allShapes;
+        shapes = allBlockShapes();
+    }
+
+    private static Set<BlockShape> allBlockShapes() {
+        return Arrays.stream(BlockType.values())
+                .flatMap(blockType -> allShapesOf(blockType))
+                .collect(Collectors.toSet());
+    }
+
+    private static Stream<BlockShape> allShapesOf(BlockType blockType) {
+        final Offsets offsets = blockType.offsets;
+        return IntStream.range(0, blockType.numberOfShapes)
+                .mapToObj(rotation -> BlockShape.from(offsets).rotate(rotation));
     }
 
     public static Set<BlockShape> all() {
