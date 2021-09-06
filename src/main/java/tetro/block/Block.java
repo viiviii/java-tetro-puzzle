3ead2e93dd6d6c02e29fb4141e588483b6c19dfb
viiviii <viiiix.viiviii@gmail.com>
2021-09-06 11:45:41 +0900

    :recycle: refactor: Block에서 type 제거
    - BlockShape가 갖고있는게 나은듯

--- src/main/java/tetro/block/Block.java
+++ src/main/java/tetro/block/Block.java
@@ -3,26 +3,15 @@ package tetro.block;
 import tetro.shape.BlockShape;
 
 public abstract class Block {
-    private final BlockType type;
     private final BlockShape shape;
 
-    protected Block(BlockType type, BlockShape shape) {
-        this.type = type;
+    protected Block(BlockShape shape) {
         this.shape = shape;
-        System.out.println("this = " + this); // TODO: 제거
-    }
-
-    public BlockType type() {
-        return type;
-    }
-
-    public BlockShape shape() {
-        return shape;
     }
 
     // TODO
     @Override
     public String toString() {
-        return "Block{" + "type=" + type + ", shape=" + shape + '}';
+        return "Block{" + "shape=" + shape.toGridString() + '}';
     }
 }
