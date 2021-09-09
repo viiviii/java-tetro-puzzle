a9ee2b5410f6111c33aeba6fea9ea41bd9e59293
viiviii <viiiix.viiviii@gmail.com>
2021-09-09 19:09:20 +0900

    :white_check_mark: test: BlockShape validate 테스트 추가

--- src/test/java/tetro/shape/BlockShapeTest.java
+++ src/test/java/tetro/shape/BlockShapeTest.java
@@ -1,17 +1,34 @@
 package tetro.shape;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tetro.block.BlockType;
+import tetro.offset.Offset;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BlockShapeTest {
+    final int SIZE = BlockShape.SIZE;
+
+    BlockShape oBlockShape;
+    BlockShape sBlockShape;
+
+    @BeforeEach
+    public void beforeEach() {
+        final int NOT_ROTATION = 0;
+        BlockShapeData data = new BlockShapeData();
+        BlockShapes blockShapes = new BlockShapes(data);
+        oBlockShape = blockShapes.get(BlockType.O).get(NOT_ROTATION);
+        sBlockShape = blockShapes.get(BlockType.S).get(NOT_ROTATION);
+    }
 
     @Test
-    public void equals_sameShapes_returnsTrue() throws Exception {
+    public void equals_sameOffsets_returnsTrue() throws Exception {
         //given
-        BlockShape shape1 = BlockShape.from(BlockType.O).rotate(3);
-        BlockShape shape2 = BlockShape.from(BlockType.O).rotate(3);
+        BlockShape shape1 = oBlockShape;
+        BlockShape shape2 = new BlockShape(List.of(0, 0, 1, 0, 0, 1, 1, 1));
 
         //when
         boolean actual = shape1.equals(shape2);
@@ -21,10 +38,10 @@ public class BlockShapeTest {
     }
 
     @Test
-    public void equals_differentShapes_returnsFalse() throws Exception {
+    public void equals_differentOffsets_returnsFalse() throws Exception {
         //given
-        BlockShape shape1 = BlockShape.from(BlockType.S).rotate(1);
-        BlockShape shape2 = BlockShape.from(BlockType.Z).rotate(2);
+        BlockShape shape1 = oBlockShape;
+        BlockShape shape2 = sBlockShape;
 
         //when
         boolean actual = shape1.equals(shape2);
@@ -34,12 +51,58 @@ public class BlockShapeTest {
     }
 
     @Test
+    public void validate_lengthIsNotEight_throwsException() throws Exception {
+        //given
+        List lengthIsNotEight = List.of(0, 0, 1, 0, 0, 1);
+
+        //then
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> new BlockShape(lengthIsNotEight));
+    }
+
+    @Test
+    public void validate_duplicateValue_throwsException() throws Exception {
+        //given
+        Offset offset = Offset.ZERO;
+        List lengthIsNotEight = List.of(offset.x, offset.y, 1, 0, 0, 1, offset.x, offset.y);
+
+        //then
+        assertThrows(IllegalArgumentException.class, () -> new BlockShape(lengthIsNotEight));
+    }
+
+    @Test
+    public void validate_containSameNumberAsSize_throwsException() throws Exception {
+        //given
+        int sameNumberAsSize = SIZE;
+        List list = List.of(0, 0, 1, 0, 0, 1, 1, sameNumberAsSize);
+
+        //then
+        assertThrows(IllegalArgumentException.class, () -> new BlockShape(list));
+    }
+
+    @Test
+    public void validate_containNumberGreaterThanSize_throwsException() throws Exception {
+        //given
+        int numberGreaterThanZie = SIZE + 1;
+        List list = List.of(0, 0, 1, 0, numberGreaterThanZie, 1, 1, 1);
+
+        //then
+        assertThrows(IllegalArgumentException.class, () -> new BlockShape(list));
+    }
+
+    @Test
+    public void validate_containNegativeNumber_throwsException() throws Exception {
+        //given
+        int negativeNumber = -1;
+        List list = List.of(negativeNumber, 0, 1, 0, 0, 1, 1, 1);
+
+        //then
+        assertThrows(IllegalArgumentException.class, () -> new BlockShape(list));
+    }
+
+    @Test
     public void toGridString_startedZeroOffset_doesNotThrowsException() throws Exception {
         //given
-        BlockType type = BlockType.O;
-
-        //when
-        BlockShape shape = BlockShape.from(type).rotate(4);
+        BlockShape shape = oBlockShape;
 
         //then
         assertDoesNotThrow(() -> shape.toGridString());
@@ -48,10 +111,7 @@ public class BlockShapeTest {
     @Test
     public void toGridString_notStartedZeroOffset_doesNotThrowsException() throws Exception {
         //given
-        BlockType type = BlockType.S;
-
-        //when
-        BlockShape shape = BlockShape.from(type).rotate(2);
+        BlockShape shape = sBlockShape;
 
         //then
         assertDoesNotThrow(() -> shape.toGridString());
