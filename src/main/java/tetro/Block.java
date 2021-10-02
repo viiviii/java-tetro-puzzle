a468257604e8d022ae9905c07a3bf0f713b54875
viiviii <viiiix.viiviii@gmail.com>
2021-10-02 13:01:59 +0900

    :recycle: refactor: Block이 AbstractGrid를 상속하도록

---
+++ src/main/java/tetro/Block.java
@@ -0,0 +1,73 @@
+package tetro;
+
+import tetro.grid.AbstractGrid;
+import tetro.grid.cells.AbstractFillCells;
+import tetro.offset.Offset;
+import tetro.offset.Offsets;
+
+import java.util.Objects;
+
+public final class Block extends AbstractGrid<Block.Shape> {
+
+    private final BlockType type;
+    private final int rotation;
+
+    private Block(Shape shape, BlockType type, int rotation) {
+        super(Shape.SIZE, shape);
+        this.type = type;
+        this.rotation = rotation;
+    }
+
+    public Block(Offsets offsets, BlockType type, int rotation) {
+        this(new Shape(offsets), type, rotation);
+    }
+
+    public BlockType type() {
+        return this.type;
+    }
+
+    public int rotation() {
+        return this.rotation;
+    }
+
+    public Block translateTo(Offset offset) {
+        final Offsets translateOffsets = this.cells().offsets().translateTo(offset);
+        return new Block(translateOffsets, this.type, this.rotation);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Block)) return false;
+        if (!super.equals(o)) return false;
+        Block other = (Block) o;
+        return rotation == other.rotation && type == other.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), type, rotation);
+    }
+
+    protected static final class Shape extends AbstractFillCells {
+        private static final int SIZE = 4;
+
+        private final Offsets offsets;
+
+        public Shape(Offsets offsets) {
+            validate(offsets);
+            this.offsets = offsets;
+        }
+
+        private static void validate(Offsets offsets) {
+            if (offsets.size() == SIZE) return;
+            throw new IllegalArgumentException("'offsets.size()' is not equal to SIZE: "
+                    + "<offsets size> " + offsets.size() + ", <SIZE> " + SIZE);
+        }
+
+        @Override
+        public Offsets offsets() {
+            return this.offsets;
+        }
+    }
+}
