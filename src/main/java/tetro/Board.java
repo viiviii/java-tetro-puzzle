12692571d6e6b83b22d4237e342784e5bc63e7e2
viiviii <viiiix.viiviii@gmail.com>
2021-10-02 13:05:11 +0900

    :recycle: refactor: Board가 AbstractGrid를 상속하도록

--- src/main/java/tetro/Board.java
+++ src/main/java/tetro/Board.java
@@ -1,27 +1,89 @@
 package tetro;
 
+import tetro.grid.AbstractGrid;
+import tetro.grid.cells.AbstractEmptyCells;
+import tetro.grid.cells.AbstractFillCells;
+import tetro.offset.Offset;
 import tetro.offset.Offsets;
 
-public final class Board {
-    public static final int LENGTH = 9;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-    private final Offsets holes;
+public final class Board extends AbstractGrid<Board.EmptyCells> {
 
-    private Board(Offsets holes) {
-        this.holes = holes;
+    private Board(int length, EmptyCells cells) {
+        super(length, cells);
     }
 
-    public static Board from(Offsets holes) {
-        // TODO
-        return new Board(holes);
+    public Board(Offsets offsets) {
+        this(9, new EmptyCells(offsets));
     }
 
-    public boolean filled() {
-        return holes.size() == 0;
+    public Set<Set<Block>> allFitCombinations() {
+        final Set<Set<Block>> combinations = combinationsFitOf(this.cells());
+        return combinations;
     }
 
-    @Override
-    public String toString() {
-        return "Board{" +"holes=" + holes + '}';
+    // TODO: 메서드명
+    private Set<Set<Block>> combinationsFitOf(EmptyCells emptyCells) {
+        final Set<Set<Block>> result = new HashSet<>();
+        if (emptyCells.isNone()) return result;
+
+        // todo: blocksFittingTo랑 fit이 떨어져있음
+        final Set<Block> blocks = blocksFittingTo(emptyCells);
+
+        for (Block block : blocks) {
+            final EmptyCells remainingEmptyCells = emptyCells.fit(block.cells());
+
+            if (remainingEmptyCells.isNone()) {
+                result.add(blocks);
+                return result;
+            }
+            Set<Set<Block>> combinations = combinationsFitOf(remainingEmptyCells);
+            if (combinations.isEmpty()) continue;
+
+            for (Set<Block> combination : combinations) {
+                combination.add(block);
+            }
+            result.addAll(combinations);
+        }
+        return result;
+    }
+
+    // TODO: 메서드명, unmodifiableSet?
+    private Set<Block> blocksFittingTo(EmptyCells emptyCells) {
+        final Offset firstEmptyCellOffset = emptyCells.offsets().first();
+        return Blocks.all().stream()
+                .map(block -> block.translateTo(firstEmptyCellOffset))
+                .filter(block -> emptyCells.canFit(block.cells()))
+                .collect(Collectors.toSet());
+    }
+
+    protected static final class EmptyCells extends AbstractEmptyCells {
+        private final Offsets offsets;
+
+        public EmptyCells(Offsets offsets) {
+            this.offsets = offsets;
+        }
+
+        // todo: 메서드명
+        public boolean isNone() {
+            return this.offsets().size() == 0;
+        }
+
+        public boolean canFit(AbstractFillCells other) {
+            return this.offsets().containsAll(other.offsets());
+        }
+
+        public EmptyCells fit(AbstractFillCells other) {
+            final Offsets remaining = this.offsets().difference(other.offsets());
+            return new EmptyCells(remaining);
+        }
+
+        @Override
+        public Offsets offsets() {
+            return this.offsets;
+        }
     }
 }
