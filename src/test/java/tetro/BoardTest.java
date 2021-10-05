f3de58b2f5c7bedaba73a55e6d07f030da5e39ef
viiviii <viiiix.viiviii@gmail.com>
2021-10-05 17:03:43 +0900

    :white_check_mark: test: 변경된 구조로 테스트 수정

--- src/test/java/tetro/BoardTest.java
+++ src/test/java/tetro/BoardTest.java
@@ -7,6 +7,7 @@ import tetro.offset.Offsets;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,9 +19,10 @@ public class BoardTest {
         //given
         Offsets unfitOffsets = Offsets.of(Offset.of(0, 0));
         Board board = new Board(unfitOffsets);
+        Puzzle puzzle = new Puzzle(board);
 
         //when
-        Set<Set<Block>> allCombinations = board.allFitCombinations();
+        Set<Puzzle.FittedBlocks> allCombinations = PuzzleSolver.allFitCombinations(puzzle);
         int actual = allCombinations.size();
 
         //then
@@ -32,9 +34,10 @@ public class BoardTest {
         //given
         Offsets emptyOffsets = Blocks.get(BlockType.O, 0).cells().offsets();
         Board board = new Board(emptyOffsets);
+        Puzzle puzzle = new Puzzle(board);
 
         //when
-        Set<Set<Block>> allCombinations = board.allFitCombinations();
+        Set<Puzzle.FittedBlocks> allCombinations = PuzzleSolver.allFitCombinations(puzzle);
         int actual = allCombinations.size();
 
         //then
@@ -54,25 +57,30 @@ public class BoardTest {
                 Offset.of(3, 4), Offset.of(4, 4), Offset.of(5, 4), Offset.of(6, 4)
         );
         Board board = new Board(emptyOffsets);
+        Puzzle puzzle = new Puzzle(board);
 
-        Set<Set<Block>> expect = new HashSet<>();
-        Block oBlock1 = Blocks.get(BlockType.O, 0).translateTo(Offset.of(3, 3));
-        Block oBlock2 = Blocks.get(BlockType.O, 0).translateTo(Offset.of(5, 3));
-        Block iBlock1 = Blocks.get(BlockType.I, 1).translateTo(Offset.of(3, 3));
-        Block iBlock2 = Blocks.get(BlockType.I, 1).translateTo(Offset.of(3, 4));
-        Block lBlock1 = Blocks.get(BlockType.L, 3).translateTo(Offset.of(3, 3));
-        Block lBlock2 = Blocks.get(BlockType.L, 1).translateTo(Offset.of(6, 3));
-        Block jBlock1 = Blocks.get(BlockType.J, 3).translateTo(Offset.of(3, 3));
-        Block jBlock2 = Blocks.get(BlockType.J, 1).translateTo(Offset.of(4, 3));
+        Set<Set<PuzzleBlock.State>> expect = new HashSet<>();
+        PuzzleBlock.State oBlock1 = new PuzzleBlock.State(BlockType.O, 0, Offset.of(3, 3));
+        PuzzleBlock.State oBlock2 = new PuzzleBlock.State(BlockType.O, 0, Offset.of(5, 3));
+        PuzzleBlock.State iBlock1 = new PuzzleBlock.State(BlockType.I, 1, Offset.of(3, 3));
+        PuzzleBlock.State iBlock2 = new PuzzleBlock.State(BlockType.I, 1, Offset.of(3, 4));
+        PuzzleBlock.State lBlock1 = new PuzzleBlock.State(BlockType.L, 3, Offset.of(3, 3));
+        PuzzleBlock.State lBlock2 = new PuzzleBlock.State(BlockType.L, 1, Offset.of(6, 3));
+        PuzzleBlock.State jBlock1 = new PuzzleBlock.State(BlockType.J, 3, Offset.of(3, 3));
+        PuzzleBlock.State jBlock2 = new PuzzleBlock.State(BlockType.J, 1, Offset.of(4, 3));
         expect.add(Set.of(oBlock1, oBlock2));
         expect.add(Set.of(iBlock1, iBlock2));
         expect.add(Set.of(lBlock1, lBlock2));
         expect.add(Set.of(jBlock1, jBlock2));
 
         //when
-        Set<Set<Block>> allCombinations = board.allFitCombinations();
+        Set<Puzzle.FittedBlocks> allCombinations = PuzzleSolver.allFitCombinations(puzzle);
+        Set<Set<PuzzleBlock.State>> actual = allCombinations
+                .stream().map(e -> e.blockStates())
+                .collect(Collectors.toSet());
 
         //then
-        assertEquals(expect, allCombinations);
+        assertEquals(expect, actual);
+        assertEquals(4, actual.size());
     }
 }
