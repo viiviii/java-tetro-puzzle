3d19e5be768c9967b161fceaa77d51c5d8c54e16
viiviii <viiiix.viiviii@gmail.com>
2021-10-11 15:20:08 +0900

    :fire: remove: FitBlock.State inner class 제거

--- src/main/java/tetro/FitBlock.java
+++ src/main/java/tetro/FitBlock.java
@@ -21,11 +21,6 @@ public final class FitBlock extends AbstractNonBlankCells {
         return this.block.shape().offsets().translateTo(offsetInThePuzzle);
     }
 
-    // todo: 메서드명, 클래스명 fillState랑 헷갈림
-    public FitBlock.State blockState() {
-        return new State(this.block, this.offsetInThePuzzle);
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -38,35 +33,5 @@ public final class FitBlock extends AbstractNonBlankCells {
     @Override
     public int hashCode() {
         return Objects.hash(super.hashCode(), block, offsetInThePuzzle);
-    }
-
-    public static final class State {
-        private final BlockType type;
-        private final int rotation;
-        private final Offset offset;
-
-        // for testing
-        public State(BlockType type, int rotation, Offset offset) {
-            this.type = type;
-            this.rotation = rotation;
-            this.offset = offset;
-        }
-
-        public State(Block block, Offset offset) {
-            this(block.type(), block.rotation(), offset);
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (!(o instanceof State)) return false;
-            State that = (State) o;
-            return rotation == that.rotation && type == that.type && offset.equals(that.offset);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(type, rotation, offset);
-        }
     }
 }
